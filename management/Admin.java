a496d2f6863e929af3ed5ac9a70fad8e9470c4f5
obelisk0114 <obelisk0114@yahoo.com.tw>
2016-07-27 04:52:34 +0800

    Finish GUI
    Finish GUI

--- management/Admin.java
+++ management/Admin.java
@@ -6,16 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JPopupMenu;
+import javax.swing.SwingConstants;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.GridLayout;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -24,25 +30,53 @@ public class Admin {
 	final int height = 550;
 	
 	private JFrame mainFrame;
-	private JPanel totalTable, headerTable, staffTable;
+	private JPanel frameTable;
 	private JScrollPane containTable;
 	private List<JLabel> item = new ArrayList<JLabel>(50);
-	private ArrayList<List<JLabel>> personLeave = new ArrayList<List<JLabel>>();
+	private List<JLabel> personName = new ArrayList<JLabel>();
+	private ArrayList<List<JButton>> personLeave = new ArrayList<List<JButton>>();
 	private ArrayList<List<JButton>> person = new ArrayList<List<JButton>>();
+	private JMenuBar menuBar;
+	private JMenu modeMenu, helpMenu;
+	private JMenuItem openMenuItem, saveMenuItem, helpMenuItem;
+	
+	int tag = -1;
+	int maxSize = -1;
 	
 	int maxSize = -1;
 	
 	public Admin(String title) {
 		// Run
 		mainFrame = new JFrame(title);
-		totalTable = new JPanel();
-		headerTable = new JPanel();
-		staffTable = new JPanel();
-		containTable = new JScrollPane(totalTable); 
+		frameTable = new JPanel();
+		containTable = new JScrollPane(frameTable); 
+		
+		menuBar = new JMenuBar();
+		modeMenu = new JMenu("File");
+		helpMenu = new JMenu("Help");
+		fileSystem fileControl = new fileSystem();
+		openMenuItem = new JMenuItem("Open");
+		openMenuItem.addActionListener(fileControl);
+		saveMenuItem = new JMenuItem("Save");
+		saveMenuItem.addActionListener(fileControl);
+		helpMenuItem = new JMenuItem("About");
+		helpMenuItem.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showOptionDialog(null, "Â²¤¶:\n    "
+						+ "½Ğ±N excel ¦s¦¨ csv ÀÉ®×¦A¶}±Ò\n"
+						+ "¸ô®|½d¨Ò:\n    C:/Users/LHC/Desktop/test.csv", 
+						"Ãö©ó  -- by obelisk0114", JOptionPane.DEFAULT_OPTION,
+						JOptionPane.INFORMATION_MESSAGE, null, null, null);
+					}
+				}
+				);
 		
 		//ArrayList<List<String>> dataTable = readCSVToArrayList("C:/Users/TEMP/Desktop/test.csv");
 		ArrayList<List<String>> dataTable = readCSVToArrayList("C:/Users/15T-J000/Desktop/test.csv");
 		filledEmptySpace(dataTable);
+		setTable(dataTable);
+		paintTable(dataTable);
 	}
 	
 	public ArrayList<List<String>> readCSVToArrayList (String csvpath) {
@@ -51,22 +85,22 @@ public class Admin {
 		BufferedReader reader;
 		try {
 			reader = new BufferedReader(new FileReader(csvpath));
-//          reader.readLine();// æ˜¯å¦è®€å–ç¬¬ä¸€è¡Œ (åŠ ä¸Šè¨»è§£ä»£è¡¨æœƒè®€å–,è¨»è§£æ‹¿æ‰ä¸æœƒè®€å–)
-			String line = null;// æš«å­˜ç”¨(æ¸¬è©¦æ˜¯å¦å·²è®€å®Œæª”)
+//          reader.readLine();// ¬O§_Åª¨ú²Ä¤@¦æ (¥[¤Wµù¸Ñ¥Nªí·|Åª¨ú,µù¸Ñ®³±¼¤£·|Åª¨ú)
+			String line = null;// ¼È¦s¥Î(´ú¸Õ¬O§_¤wÅª§¹ÀÉ)
 			
 			 // read data
             while ((line = reader.readLine()) != null) {
                 
-                //å­˜æ”¾æ¯ä¸€åˆ—è³‡æ–™å…§å®¹(æ©«çš„)
+                //¦s©ñ¨C¤@¦C¸ê®Æ¤º®e(¾îªº)
                 ArrayList<String> ticketStr = new ArrayList<String>();
                 
-                String[] item = line.split(",");//csvæ–‡ä»¶ç‚ºä¾æ“šé€—è™Ÿåˆ‡å‰²
+                String[] item = line.split(",");//csv¤å¥ó¬°¨Ì¾Ú³r¸¹¤Á³Î
                 
-                //æ¸…é™¤ä¸Šä¸€æ¬¡å­˜å…¥çš„è³‡æ–™
+                //²M°£¤W¤@¦¸¦s¤Jªº¸ê®Æ
                 ticketStr.clear();
                 
                 //System.out.println(item.length);
-                //è®€æª”(å–®åˆ—è³‡æ–™)
+                // Read file (single data)
                 for(int i = 0; i < item.length; i++){
                     
                     ticketStr.add(item[i]);
@@ -120,15 +154,159 @@ public class Admin {
 			filledEmptySpace(inputTable);
 	}
 	
+	public void setTable(ArrayList<List<String>> inputTable) {
+		for (int i = 0; i < inputTable.get(0).size(); i++) {
+			String element = inputTable.get(0).get(i);
+			if (element.equals(null)) {
+				element = "";
+			}
+			
+			if (element.contains("°²")) {
+				tag = i;
+			}
+			
+			item.add(new JLabel(element, SwingConstants.CENTER));
+		}
+		
+		for (int i = 1; i < inputTable.size(); i++) {
+			personName.add(new JLabel(inputTable.get(i).get(0)));
+			
+			List<JButton> personLeaveRow = new ArrayList<JButton>();
+			List<JButton> personRow = new ArrayList<JButton>(31);
+			
+			for (int j = 1; j < inputTable.get(i).size(); j++) {
+				String tempLeaveData = inputTable.get(i).get(j);
+				if (tempLeaveData.equals(null)) {
+					tempLeaveData = "";
+				}
+				
+				if (j >= tag && tag != -1) {	
+					personLeaveRow.add(new JButton(tempLeaveData));
+				}
+				else {
+					personRow.add(new JButton(tempLeaveData));
+				}
+			}
+			
+			personLeave.add(personLeaveRow);
+			person.add(personRow);
+		}
+	}
+	
+	public void paintTable(ArrayList<List<String>> inputTable) {
+		frameTable.setLayout(new GridBagLayout());
+		int gridRecord = 2;
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.insets = new Insets(0,0,0,0);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		if (item.get(0).equals("")) {
+			gbc.ipadx = 10;
+			gbc.ipady = 10;
+			gbc.gridwidth = gridRecord;
+		    gbc.gridheight = gridRecord;
+		}
+		else {
+			gridRecord = 2;
+			gbc.ipadx = 20;
+			gbc.ipady = 20;
+			gbc.gridwidth = gridRecord;
+		    gbc.gridheight = gridRecord;
+		}
+		frameTable.add(item.get(0), gbc);
+		item.get(0).setVisible(true);
+		
+		for (int i = 1; i < item.size(); i++) {
+			gbc.weightx = 1;
+			gbc.weighty = 1;
+			gbc.gridwidth = 2;
+			gbc.gridheight = 2;
+			gbc.ipadx = 20;
+			gbc.ipady = 20;
+			gbc.gridx = gridRecord + (i - 1) * 2;
+			gbc.gridy = 0;
+			frameTable.add(item.get(i), gbc);
+			item.get(i).setVisible(true);
+		}
+
+		for (int i = 0; i < personName.size(); i++) {
+			gbc.weightx = 1;
+			gbc.weighty = 1;
+			gbc.gridwidth = 2;
+			gbc.gridheight = 2;
+			gbc.ipadx = 20;
+			gbc.ipady = 20;
+			gbc.gridx = 0;
+			gbc.gridy = gridRecord + i * 2;
+		    frameTable.add(personName.get(i), gbc);
+		    personName.get(i).setVisible(true);
+		}
+
+		int controlSize = tag;
+		boolean tagCondition = true;
+		if (controlSize < 0) {
+			controlSize = maxSize;
+			tagCondition = false;
+		}
+		System.out.println("tag : " + tag);
+		System.out.println("maxSize : " + maxSize);
+		System.out.println("controlSize : " + controlSize);
+		
+		for (int i = 0; i < inputTable.size() - 1; i++) {
+			for (int j = 0; j < controlSize - 1; j++) {
+				gbc.weightx = 1;
+				gbc.weighty = 1;
+				gbc.gridwidth = 2;
+				gbc.gridheight = 2;
+				gbc.ipadx = 20;
+				gbc.ipady = 20;
+				gbc.gridx = gridRecord + j * 2;
+				gbc.gridy = gridRecord + i * 2;
+			    frameTable.add(person.get(i).get(j), gbc);
+			    person.get(i).get(j).setVisible(true);
+				//person.get(i).get(j).addActionListener(l);
+			}
+		}
+
+		if (tagCondition == true) {
+			for (int i = 0; i < inputTable.size() - 1; i++) {
+				for (int j = 0; j < maxSize - controlSize; j++) {
+					gbc.weightx = 1;
+					gbc.weighty = 1;
+					gbc.gridwidth = 2;
+					gbc.gridheight = 2;
+					gbc.ipadx = 20;
+					gbc.ipady = 20;
+					gbc.gridx = gridRecord + (j + tag - 1) * 2;
+					gbc.gridy = gridRecord + i * 2;
+				    frameTable.add(personLeave.get(i).get(j), gbc);
+				    personLeave.get(i).get(j).setEnabled(false);
+				    personLeave.get(i).get(j).setVisible(true);
+				}
+			}
+		}
+	}
+	
+	private class fileSystem implements ActionListener {
+		public void actionPerformed(ActionEvent ae) {
+			//
+		}
+	}
+	
 	public void launchFrame() {
 		mainFrame.setSize(width, height);
-		
-		totalTable.setLayout(new GridLayout(2, 1));
-		totalTable.add(headerTable);
-		totalTable.add(staffTable);
-		totalTable.setVisible(true);
+		mainFrame.add(menuBar);
+		menuBar.add(modeMenu);
+		menuBar.add(helpMenu);
+		modeMenu.add(openMenuItem);
+		modeMenu.add(saveMenuItem);
+		helpMenu.add(helpMenuItem);
 		
 		mainFrame.add(containTable);
+		mainFrame.setJMenuBar(menuBar);
 		mainFrame.setLocation(350, 150);
 		mainFrame.setVisible(true);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -148,7 +326,7 @@ public class Admin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Admin ad = new Admin("è«‹å‡ç®¡ç†ç³»çµ±");
+		Admin ad = new Admin("½Ğ°²ºŞ²z¨t²Î");
 		ad.launchFrame();
 
 	}
